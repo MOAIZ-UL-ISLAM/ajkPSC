3b649d08d962c104c68c426521a1e66980005e73
@moaiz0404 <moaiz0404@gmail.com>
2025-01-22 20:25:33 +0500

    Refactor '/smith' route response and improve code formatting

--- Jobs-App/src/app.ts
+++ Jobs-App/src/app.ts
@@ -1,30 +1,27 @@
-import express from 'express';
-import authRoutes from './routes/auth.routes';
-import jobAppRoutes from './routes/jobApplication.routes';
-import cors from 'cors'
-import dotenv from 'dotenv';
+import express from "express";
+import authRoutes from "./routes/auth.routes";
+import jobAppRoutes from "./routes/jobApplication.routes";
+import cors from "cors";
+import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
 
-app.get('/smith', (req, res) => {
-  res.send('<h1>Hello, world!</h1>');
+app.get("/smith", (req, res) => {
+  res.send("<h1>world!</h1>");
 });
 
-
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  }));
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
-app.use('/api/auth', authRoutes);
-app.use('',jobAppRoutes)
-
-
-
-
+app.use("/api/auth", authRoutes);
+app.use("", jobAppRoutes);
 
 export default app;
